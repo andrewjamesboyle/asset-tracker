36ec190344251d2b40031f0f8bb2db033feaa7ea
kyle-liminal <kyle@liminal.sh>
2023-12-05 13:59:25 -0800

    testing matchList

--- api/src/services/matchList.ts
+++ api/src/services/matchList.ts
@@ -1,10 +1,26 @@
 import fetch from 'node-fetch'
 
-export async function matchList(listId: number, contacts: any[]) {
-  const response = await fetch(`https://api.propertyradar.com/v1/lists/${listId}/import/items`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.PROPERTY_RADAR_API_KEY}`,
-  })
+// interface matchListArgs {
+//   listId: number
+// }
+
+export const matchList = async (listId, contacts) => {
+  const query = new URLSearchParams({
+    Purchase: '0',
+    Fields: 'ListImportItemID',
+  }).toString()
+  const response = await fetch(
+    `https://api.propertyradar.com/v1/lists/${listId}/import/items?${query}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.PROPERTY_RADAR_API_KEY}`,
+      },
+      body: JSON.stringify({ contacts: contacts }),
+    }
+  )
+  console.log(response)
+
+  // Handle the response as needed
 }
