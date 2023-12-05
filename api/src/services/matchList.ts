d1eac9dc75e37a8869be4a98fed6d01f1eda4050
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-12-04 16:43:28 -0800

    begin building match endpoint

---
+++ api/src/services/matchList.ts
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch'
+
+export async function matchList(listId: number, contacts: any[]) {
+  const response = await fetch(`https://api.propertyradar.com/v1/lists/${listId}/import/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.PROPERTY_RADAR_API_KEY}`,
+  })
+}
