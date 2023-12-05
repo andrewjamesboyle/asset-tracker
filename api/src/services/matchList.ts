7f00bea3fb423fbcd901d2ae5cf9ec02ff2b1749
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-12-05 14:15:44 -0800

    matchList endpoint works

--- api/src/services/matchList.ts
+++ api/src/services/matchList.ts
@@ -9,18 +9,21 @@ export const matchList = async (listId, contacts) => {
     Purchase: '0',
     Fields: 'ListImportItemID',
   }).toString()
-  const response = await fetch(
-    `https://api.propertyradar.com/v1/lists/${listId}/import/items?${query}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.PROPERTY_RADAR_API_KEY}`,
-      },
-      body: JSON.stringify({ contacts: contacts }),
-    }
-  )
-  console.log(response)
-
-  // Handle the response as needed
+  try {
+    const response = await fetch(
+      `https://api.propertyradar.com/v1/lists/${listId}/import/items?${query}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.PROPERTY_RADAR_API_KEY}`,
+        },
+        body: JSON.stringify(contacts),
+      }
+    )
+    const data = await response.json()
+    console.log('data', data)
+  } catch (error) {
+    console.error('Request Error:', error)
+  }
 }
